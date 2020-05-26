2548a12604413e4dad00bb41a676e3d56c51fe02
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-05-26 19:16:31 +0530

    Added : Stored Employee Wage Day And Dailywage With Total Wage

--- empWage.sh
+++ empWage.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+
 
 # CONSTANTS FOR THE PROGRAM
 IS_PART_TIME=1
 IS_FULL_TIME=2
-MAX_HRS_IN_MONTH=4
+MAX_HRS_IN_MONTH=100
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
 
@@ -11,6 +11,8 @@ NUM_WORKING_DAYS=20
 # VARIABLES
 totalEmpHrs=0
 totalWorkingDays=0
+
+declare -A dailWage
 
 function getWorkHrs() {
   local empCheck=$1
@@ -32,6 +34,8 @@ function getEmpWage() {
         local empHr=$1
         echo $(($empHr*$EMP_RATE_PER_HR))
 }
+echo "Welcome to Employee Wage Program"
+echo "Dayno           DailyWage       TotalWage"
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
     $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
@@ -40,8 +44,9 @@ do
    empCheck=$((RANDOM%3))
    empHrs="$( getWorkHrs $empCheck )"
    totalEmpHrs=$(($totalEmpHrs+$empHrs))
-   dailWage[$totalWorkingDays]="$( getEmpWage $empHrs )"
+   dailWage["Day "$totalWorkingDays]="$( getEmpWage $empHrs )"
 done
 
 totalSalery=$(($totalEmpHrs*$EMP_RATE_PER_HR))
 echo ${dailWage[@]}
+echo ${!dailWage[@]}
