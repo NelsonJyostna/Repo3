d56d0f93940ea7af3e20057c2572d763623c45fd
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-05-26 16:39:54 +0530

    Added : Employee Attendance

--- empWage.sh
+++ empWage.sh
@@ -1,1 +1,11 @@
-echo "welcome"
+#!/bin/bash -x
+
+isPresent=1
+randomcheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomcheck ]
+then
+    echo "Employ is Present"
+else
+    echo "Employ is not Present"
+fi
