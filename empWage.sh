ecb83e3a33535e2d6adc1db01888b69314911c1d
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-05-26 18:13:20 +0530

    Added : Employee Wage With Total Working Hours Or Day

--- empWage.sh
+++ empWage.sh
@@ -1,26 +1,36 @@
 #!/bin/bash -x
 
-isPartTime=1
-isFullTime=2
-totalSalery=0
-empRatePerHr=20
-numWorkingDays=20
 
-for (( day=1; day<=numWorkingDays; day++ ))
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+
+
+# VARIABLES
+totalEmpHrs=0
+totalWorkingDays=0
+
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+    $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-    empCheck=$((RANDOM%3))
-         case $empCheck in
-                 $isFullTime)
-                         empHrs=8
-                          ;;
-                 $isPartTime)
-                         empHrs=4
-                          ;;
-                 *)
-                         empHrs=0
-                          ;;
-         esac
-
-         salery=$(($empHrs*$empRatePerHr))
-         totalSalery=$((totalSalery+$salery))
+   ((totalWorkingDays++))
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+       $IS_FULL_TIME)
+          empHrs=8
+             ;;
+       $IS_PART_TIME)
+          empHrs=4
+             ;;
+       *)
+          empHrs=0
+             ;;
+    esac
+    totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
+
+totalSalery=$(($totalEmpHrs*$EMP_RATE_PER_HR))
