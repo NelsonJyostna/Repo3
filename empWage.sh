8f40d4e18beed4ed426df3b5dfcbcbe73e9c1293
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-05-26 18:25:15 +0530

    Added : Employee Wage Using Function

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -x
 
-
 # CONSTANTS FOR THE PROGRAM
 IS_PART_TIME=1
 IS_FULL_TIME=2
-MAX_HRS_IN_MONTH=100
+MAX_HRS_IN_MONTH=4
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
 
@@ -13,24 +12,30 @@ NUM_WORKING_DAYS=20
 totalEmpHrs=0
 totalWorkingDays=0
 
+function getWorkHrs() {
+  local empCheck=$1
+  case $empCheck in
+       $IS_FULL_TIME)
+          empHrs=8
+             ;;
+       $IS_PART_TIME)
+          empHrs=6
+             ;;
+       *)
+          empHrs=0
+             ;;
+    esac
+    echo $empHrs
+}
+
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
     $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
    ((totalWorkingDays++))
    empCheck=$((RANDOM%3))
-   case $empCheck in
-       $IS_FULL_TIME)
-          empHrs=8
-             ;;
-       $IS_PART_TIME)
-          empHrs=4
-             ;;
-       *)
-          empHrs=0
-             ;;
-    esac
-    totalEmpHrs=$(($totalEmpHrs+$empHrs))
+   empHrs="$( getWorkHrs $empCheck )"
+   totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
 
 totalSalery=$(($totalEmpHrs*$EMP_RATE_PER_HR))
